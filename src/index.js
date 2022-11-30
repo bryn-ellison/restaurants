720129ebafc3ebef8a73066e8e8656a3b0f5b304
bryn-ellison <bryn.ellison@outlook.com>
2022-11-30 15:53:55 +0000

    css bundler setup via webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!!!");
+import "./style.css";
+
+function home() {
+  const test = document.createElement("div");
+  test.textContent = "HELLO WORLD";
+  test.id = "test-div";
+
+  return test;
+}
+
+document.body.appendChild(home());
